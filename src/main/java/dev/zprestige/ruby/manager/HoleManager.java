c1ad099fc69630f4fe6ae4e0929a823b7d568357
vikkn <vikaslagerweij@gmail.com>
2022-02-22 08:20:52 +0100

    HoleManager rewrite

--- src/main/java/dev/zprestige/ruby/manager/HoleManager.java
+++ src/main/java/dev/zprestige/ruby/manager/HoleManager.java
@@ -2,86 +2,149 @@ package dev.zprestige.ruby.manager;
 
 import dev.zprestige.ruby.Ruby;
 import dev.zprestige.ruby.util.BlockUtil;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class HoleManager {
-    public ArrayList<BlockPos> obsidianHoles = new ArrayList<>();
-    public ArrayList<BlockPos> bedrockHoles = new ArrayList<>();
+    public Minecraft mc = Ruby.mc;
+    public ArrayList<HolePos> holes = new ArrayList<>();
 
-    public void onRenderWorldLastEvent() {
+    public void onRenderWorldLastEvent(){
         Ruby.threadManager.run(() -> {
-            bedrockHoles = setupBedrock();
-            obsidianHoles = setupObsidian();
+            holes = getHoles();
         });
     }
+    public Vec3i[] Hole = {
+            new Vec3i(1, 0, 0),
+            new Vec3i(-1, 0, 0),
+            new Vec3i(0, -1, 0),
+            new Vec3i(0, 0, 1),
+            new Vec3i(0, 0, -1)
+    };
 
-    public ArrayList<BlockPos> setupBedrock() {
-        ArrayList<BlockPos> posses = new ArrayList<>();
-        for (BlockPos pos : BlockUtil.getSphere(50.0, BlockUtil.AirType.OnlyAir, Ruby.mc.player)) {
-            if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK) {
-                posses.add(pos);
-            } else if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.north().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north().down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.north().north()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north().east()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north().west()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK) {
-                posses.add(pos);
-                posses.add(pos.north());
-            } else {
-                if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos.up()).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos.west().up()).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos.down()).getBlock() != Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.west().down()).getBlock() != Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.north()).getBlock() != Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.south()).getBlock() != Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.west()).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos.east()).getBlock() != Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.west().south()).getBlock() != Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.west().north()).getBlock() != Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.west().west()).getBlock() != Blocks.BEDROCK) {
+    public Vec3i[] DoubleHoleNorth = {
+            new Vec3i(0, 0, -2),
+            new Vec3i(-1, 0, -1),
+            new Vec3i(1, 0, -1),
+            new Vec3i(0, -1, -1),
+            new Vec3i(0, -1, 0),
+            new Vec3i(-1, 0, 0),
+            new Vec3i(1, 0, 0),
+            new Vec3i(0, 0, 1)
+    };
+
+    public Vec3i[] DoubleHoleWest = {
+            new Vec3i(-2, 0, 0),
+            new Vec3i(-1, 0, 1),
+            new Vec3i(-1, 0, -1),
+            new Vec3i(-1, -1, 0),
+            new Vec3i(0, -1, 0),
+            new Vec3i(0, 0, 1),
+            new Vec3i(0, 0, -1),
+            new Vec3i(1, 0, 0)
+    };
+
+    public ArrayList<HolePos> getHoles() {
+        ArrayList<HolePos> holes = new ArrayList<>();
+        for (BlockPos pos : BlockUtil.getSphere(50, BlockUtil.AirType.OnlyAir, mc.player).stream().filter(blockPos -> mc.world.getBlockState(blockPos).getBlock().equals(Blocks.AIR)).collect(Collectors.toList())) {
+            if (isEnterable(pos)) {
+                boolean isSafe = true;
+                for (Vec3i vec3i : Hole) {
+                    if (isntSafe(pos.add(vec3i))) {
+                        isSafe = false;
+                    }
+                }
+                if (isSafe) {
+                    holes.add(new HolePos(pos, Type.Bedrock));
                     continue;
                 }
-                posses.add(pos);
-                posses.add(pos.west());
-            }
-        }
-        if (!posses.isEmpty()) {
-            return posses;
-        }
-        return null;
-    }
-
-    public ArrayList<BlockPos> setupObsidian() {
-        ArrayList<BlockPos> posses = new ArrayList<>();
-        for (BlockPos pos : BlockUtil.getSphere(50.0, BlockUtil.AirType.OnlyAir, Ruby.mc.player)) {
-            if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK) {
-                ignoreStatement();
-            } else if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos).getBlock() == Blocks.AIR && (Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.OBSIDIAN) && (Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.BEDROCK) && (Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.BEDROCK) && (Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.BEDROCK) && (Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK)) {
-                posses.add(pos);
-            } else if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.north().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north().down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.north().north()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north().east()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north().west()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK) {
-                ignoreStatement();
-            } else if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.north().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos).getBlock() == Blocks.AIR && (Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.OBSIDIAN) && Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.AIR && (Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.BEDROCK) && (Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.BEDROCK) && (Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK) && (Ruby.mc.world.getBlockState(pos.north().north()).getBlock() == Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.north().north()).getBlock() == Blocks.OBSIDIAN) && (Ruby.mc.world.getBlockState(pos.north().east()).getBlock() == Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.north().east()).getBlock() == Blocks.OBSIDIAN) && (Ruby.mc.world.getBlockState(pos.north().west()).getBlock() == Blocks.BEDROCK || Ruby.mc.world.getBlockState(pos.north().west()).getBlock() == Blocks.OBSIDIAN) && (Ruby.mc.world.getBlockState(pos.north().down()).getBlock() == Blocks.OBSIDIAN || Ruby.mc.world.getBlockState(pos.north().down()).getBlock() == Blocks.BEDROCK)) {
-                posses.add(pos);
-                posses.add(pos.north());
-            } else if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.west().up()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().down()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.south()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west()).getBlock() == Blocks.AIR && Ruby.mc.world.getBlockState(pos.east()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().south()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().north()).getBlock() == Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().west()).getBlock() == Blocks.BEDROCK) {
-                ignoreStatement();
-            } else {
-                if (Ruby.mc.world.getBlockState(pos.up().up()).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos.up()).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos.west().up()).getBlock() != Blocks.AIR || Ruby.mc.world.getBlockState(pos).getBlock() != Blocks.AIR || (Ruby.mc.world.getBlockState(pos.down()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.down()).getBlock() != Blocks.OBSIDIAN) || (Ruby.mc.world.getBlockState(pos.west().down()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().down()).getBlock() != Blocks.OBSIDIAN) || (Ruby.mc.world.getBlockState(pos.north()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.north()).getBlock() != Blocks.OBSIDIAN) || (Ruby.mc.world.getBlockState(pos.south()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.south()).getBlock() != Blocks.OBSIDIAN) || Ruby.mc.world.getBlockState(pos.west()).getBlock() != Blocks.AIR || (Ruby.mc.world.getBlockState(pos.east()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.east()).getBlock() != Blocks.OBSIDIAN) || (Ruby.mc.world.getBlockState(pos.west().south()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().south()).getBlock() != Blocks.OBSIDIAN) || (Ruby.mc.world.getBlockState(pos.west().north()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().north()).getBlock() != Blocks.OBSIDIAN) || (Ruby.mc.world.getBlockState(pos.west().west()).getBlock() != Blocks.BEDROCK && Ruby.mc.world.getBlockState(pos.west().west()).getBlock() != Blocks.OBSIDIAN)) {
+                boolean isUnsafe = true;
+                for (Vec3i vec3i : Hole) {
+                    if (isntUnsafe(pos.add(vec3i))) {
+                        isUnsafe = false;
+                    }
+                }
+                if (isUnsafe) {
+                    holes.add(new HolePos(pos, Type.Obsidian));
                     continue;
                 }
-                posses.add(pos);
-                posses.add(pos.west());
+                boolean isSafeDoubleNorth = true;
+                for (Vec3i vec3i : DoubleHoleNorth) {
+                    if (isntSafe(pos.add(vec3i))) {
+                        isSafeDoubleNorth = false;
+                    }
+                }
+                if (isSafeDoubleNorth) {
+                    holes.add(new HolePos(pos, Type.DoubleBedrockNorth));
+                    continue;
+                }
+                boolean isUnSafeDoubleNorth = true;
+                for (Vec3i vec3i : DoubleHoleNorth) {
+                    if (isntUnsafe(pos.add(vec3i))) {
+                        isUnSafeDoubleNorth = false;
+                    }
+                }
+                if (isUnSafeDoubleNorth) {
+                    holes.add(new HolePos(pos, Type.DoubleObsidianNorth));
+                    continue;
+                }
+                boolean isSafeDoubleWest = true;
+                for (Vec3i vec3i : DoubleHoleWest) {
+                    if (isntUnsafe(pos.add(vec3i))) {
+                        isSafeDoubleWest = false;
+                    }
+                }
+                if (isSafeDoubleWest) {
+                    holes.add(new HolePos(pos, Type.DoubleBedrockWest));
+                    continue;
+                }
+                boolean isUnSafeDoubleWest = true;
+                for (Vec3i vec3i : DoubleHoleWest) {
+                    if (isntUnsafe(pos.add(vec3i))) {
+                        isUnSafeDoubleWest = false;
+                    }
+                }
+                if (isUnSafeDoubleWest) {
+                    holes.add(new HolePos(pos, Type.DoubleObsidianWest));
+                }
             }
         }
-        if (!posses.isEmpty()) {
-            return posses;
+        return holes;
+    }
+
+    public boolean isEnterable(BlockPos pos) {
+        return mc.world.getBlockState(pos.up()).getBlock().equals(Blocks.AIR) && mc.world.getBlockState(pos.up().up()).getBlock().equals(Blocks.AIR);
+    }
+
+    public boolean isntUnsafe(BlockPos pos) {
+        return !mc.world.getBlockState(pos).getBlock().equals(Blocks.BEDROCK) && !mc.world.getBlockState(pos).getBlock().equals(Blocks.OBSIDIAN);
+    }
+
+    public boolean isntSafe(BlockPos pos) {
+        return !mc.world.getBlockState(pos).getBlock().equals(Blocks.BEDROCK);
+    }
+
+    public static class HolePos {
+        public BlockPos pos;
+        public Type holeType;
+
+        public HolePos(BlockPos pos, Type holeType ) {
+            this.pos = pos;
+            this.holeType = holeType;
         }
-        return null;
     }
 
-    public void ignoreStatement() {
-    }
-
-    public ArrayList<BlockPos> getObsidianHoles(double radius) {
-        if (obsidianHoles != null && !obsidianHoles.isEmpty())
-            return obsidianHoles.stream().filter(pos -> Ruby.mc.player.getDistanceSq(pos) < (radius * radius)).collect(Collectors.toCollection(ArrayList::new));
-        return null;
-
-    }
-
-    public ArrayList<BlockPos> getBedrockHoles(double radius) {
-        if (bedrockHoles != null && !bedrockHoles.isEmpty())
-            return bedrockHoles.stream().filter(pos -> Ruby.mc.player.getDistanceSq(pos) < (radius * radius)).collect(Collectors.toCollection(ArrayList::new));
-        return null;
+    public enum Type {
+        Bedrock,
+        Obsidian,
+        DoubleBedrockNorth,
+        DoubleBedrockWest,
+        DoubleObsidianNorth,
+        DoubleObsidianWest
     }
 }
